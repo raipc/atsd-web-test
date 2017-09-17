7b2537a85a081a7de0ed9968f03c7c9b96556023
Sergei Rodionov <sergei.rodionov@axibase.com>
2017-09-17 23:38:11 +0300

    entity-1 insert is no longer in insert_user.sh

--- atsd_webtests/src/test/java/com/axibase/webtest/service/EntitiesServiceTest.java
+++ atsd_webtests/src/test/java/com/axibase/webtest/service/EntitiesServiceTest.java
@@ -4,9 +4,6 @@ package com.axibase.webtest.service;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Created by sild on 02.02.15.
- */
 public class EntitiesServiceTest extends AtstTest {
     @Test
     public void checkDefaultEntity() {
@@ -22,9 +19,9 @@ public class EntitiesServiceTest extends AtstTest {
             AtstTest.driver.navigate().to(AtstTest.url + "/entities");
             Assert.assertEquals(generateAssertMessage("Title should be 'Entities'"), AtstTest.driver.getTitle(), EntitiesService.title);
             EntitiesService es = new EntitiesService(AtstTest.driver);
-            Assert.assertTrue(generateAssertMessage("Should be more then 2 rows in entities table"), es.getEntitiesCount() > 2);
+            Assert.assertTrue(generateAssertMessage("Should be more than 1 row in entities table"), es.getEntitiesCount() > 1);
             Assert.assertNotNull(generateAssertMessage("Can't find 'atsd' cell"), es.getEntityByName("atsd"));
-            Assert.assertNotNull(generateAssertMessage("Can't find 'entity-1' cell"), es.getEntityByName("entity-1"));
+            //Assert.assertNotNull(generateAssertMessage("Can't find 'entity-1' cell"), es.getEntityByName("entity-1"));
             Assert.assertNotNull(generateAssertMessage("Can't find " + hostname + " cell"), es.getEntityByName(hostname.toLowerCase()));
         } catch (AssertionError err) {
             String filepath = AtstTest.screenshotDir + "/" +
